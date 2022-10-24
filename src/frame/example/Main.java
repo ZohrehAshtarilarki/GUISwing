c98e91d11db6b59b657156e16a8d73027034ffd1
ZohrehAshtarilarki <86028916+ZohrehAshtarilarki@users.noreply.github.com>
2022-10-24 12:57:10 -0700

    First GUI

---
+++ src/frame/example/Main.java
@@ -0,0 +1,43 @@
+package frame.example;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //MyFrame myFrame = new MyFrame(); //creates an instance of frame
+
+        // JLabel = a GUI display area for a string of text, an image, or both. Label is a component for a frame
+
+        ImageIcon image = new ImageIcon("/Users/zohrehashtarilarki/Desktop/My Codes(49J)/GUI/GUI/logo.png");
+        Border border = BorderFactory.createLineBorder(Color.red,3);
+
+        JLabel label = new JLabel(); //create a label
+        label.setText("Bro, do you even code?"); //set text of label
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT,CENTER,RIGHT of ImageIcon
+        label.setVerticalTextPosition(JLabel.TOP); //set text TOP,CENTER,BOTTOM of ImageIcon
+        label.setForeground(new Color(0xF3532F)); //set font color of text
+        label.setFont(new Font("Times New Roman", Font.BOLD, 30)); //set font of text
+        label.setIconTextGap(10); //set gap of text to image
+        label.setBackground(Color.blue); //set background color
+        label.setOpaque(true); //display background color
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.TOP); //set vertical position of icon+text within label
+        label.setHorizontalAlignment(JLabel.LEFT); //set horizontal position of icon+text within label
+        label.setBounds(700, 300, 350, 350); //set x,y position within frame as well as dimensions
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setTitle("JFrame title goes here");
+        frame.setIconImage(image.getImage());
+        frame.setLayout(null); //set the label in the defined bounds;otherwise, set it on the entire frame
+        frame.setVisible(true);
+        frame.add(label);
+        //frame.pack();
+    }
+}
---
+++ src/frame/example/MyFrame.java
@@ -0,0 +1,29 @@
+package frame.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *  // JFrame  = a GUI window to add components to
+ */
+public class MyFrame extends JFrame {
+    //constructor is called whenever we make an instance of MyFrame
+    /*
+    MyFrame() {
+        this.setTitle("JFrame title goes here"); //sets title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of the application
+        this.setSize(420, 420); //sets the x-dimension, and y-dimension of frame
+        this.setVisible(true); //make frame visible
+        // this.setResizable(false); //prevent frame from being resized
+
+        //create an instance of ImageIcon
+        ImageIcon image = new ImageIcon("/Users/zohrehashtarilarki/Desktop/My Codes(49J)/GUI/GUI/logo.png");
+        this.setIconImage(image.getImage()); //change icon of frame
+        //this.getContentPane().setBackground(Color.blue); //change color of background
+        //creating custom background color
+        this.getContentPane().setBackground(new Color(123, 50, 250)); //RGB values from 0-255
+        //this.getContentPane().setBackground(new Color(0x123456)); //hexadecimal color value from 0x000000-0xFFFFFF
+    }
+    */
+
+}
